51697358b9e51646c618a00fd7ff2af69b7a4ea9
ninja <bay.ninjaz@gmail.com>
2023-01-14 13:07:01 -0800

    update courses

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css'
 import Card from './Card'
 
 const courses = [
-  { id: 'PSYCH100', name: 'Intro to Psychology', image: 'https://www.udc.edu/social-udc/wp-content/uploads/sites/24/2018/03/Importance-of-Psychology_UDC.jpg'},
-  { id: 'MATH230', name: 'Calculus AB', image: 'https://www.thoughtco.com/thmb/irSi_hwriBZL3QbGsCiiNsgaSWI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/calculus-on-blackboard-79338340-5be4695946e0fb0026d6856f.jpg' },
-  { id: 'HIST100', name: 'US History', image: 'http://pioneerinstitute.org/wp-content/uploads/Constitution-and-flag-1024x682.jpg' }
+  {
+    id: 'PSYCH100',
+    name: 'Intro to Psychology',
+    image: 'https://www.udc.edu/social-udc/wp-content/uploads/sites/24/2018/03/Importance-of-Psychology_UDC.jpg',
+    postDate: '01/12/20',
+    capacity: 100,
+    enrolled: 55
+  },
+  {
+    id: 'MATH230',
+    name: 'Calculus AB',
+    image: 'https://www.thoughtco.com/thmb/irSi_hwriBZL3QbGsCiiNsgaSWI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/calculus-on-blackboard-79338340-5be4695946e0fb0026d6856f.jpg',
+    postDate: '01/12/20',
+    capacity: 50,
+    enrolled: 12
+  },
+  {
+    id: 'HIST100',
+    name: 'US History',
+    image: 'http://pioneerinstitute.org/wp-content/uploads/Constitution-and-flag-1024x682.jpg',
+    postDate: '01/12/20',
+    capacity: 100,
+    enrolled: 79
+  }
 ]
 
 
